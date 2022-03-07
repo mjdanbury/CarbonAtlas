8d06d2e3a04b49d5d8b94196d4aef723df372a75
Matty <mjd358@cornell.edu>
2022-03-07 10:10:10 -0600

    Initial Commit

---
+++ CarbonAtlas/CarbonAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonAtlasApp.swift
+//  CarbonAtlas
+//
+//  Created by Matthew Danbury on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CarbonAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
